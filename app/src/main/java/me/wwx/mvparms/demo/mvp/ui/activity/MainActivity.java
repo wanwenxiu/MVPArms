26851e361ebec79631f6abdd891a172662dca785
wanwenxiu <wanwenxiu0709@foxmail.com>
2017-05-22 17:43:44 +0800

    增加BottomNavigationView和ViewPage

--- app/src/main/java/me/wwx/mvparms/demo/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/me/wwx/mvparms/demo/mvp/ui/activity/MainActivity.java
@@ -3,14 +3,15 @@ package me.wwx.mvparms.demo.mvp.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -26,6 +27,8 @@ import me.wwx.mvparms.demo.di.component.DaggerMainComponent;
 import me.wwx.mvparms.demo.di.module.MainModule;
 import me.wwx.mvparms.demo.mvp.contract.MainContract;
 import me.wwx.mvparms.demo.mvp.presenter.MainPresenter;
+import me.wwx.mvparms.demo.mvp.ui.adapter.MainButtonAdapter;
+import me.wwx.mvparms.demo.mvp.ui.util.BottomNavigationViewHelper;
 
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
@@ -36,21 +39,21 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * 描述：
  */
 
-public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View, NavigationView.OnNavigationItemSelectedListener,SwipeRefreshLayout.OnRefreshListener {
-
-
+public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View, NavigationView.OnNavigationItemSelectedListener, SwipeRefreshLayout.OnRefreshListener {
     @BindView(R.id.maintoolbar)
     Toolbar maintoolbar;
-    @BindView(R.id.recycleView)
-    RecyclerView recycleView;
-    @BindView(R.id.swipeRefresh)
-    SwipeRefreshLayout swipeRefresh;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
     @BindView(R.id.fab)
     FloatingActionButton fab;
     @BindView(R.id.nav_view)
     NavigationView navView;
     @BindView(R.id.drawer_layout)
     DrawerLayout drawerLayout;
+    @BindView(R.id.viewpages)
+    ViewPager viewpages;
+
+    private MenuItem menuItem;
 
     @Override
     public void setupActivityComponent(AppComponent appComponent) {
@@ -83,67 +86,96 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
 
         navView.setNavigationItemSelectedListener(this);
 
-        swipeRefresh.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light, android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-        swipeRefresh.setOnRefreshListener(this);
+        viewpages.setAdapter(new MainButtonAdapter(getSupportFragmentManager()));
+        viewpages.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = navigation.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        viewpages.setOffscreenPageLimit(2);
+
+        BottomNavigationViewHelper.disableShiftMode(navigation);
     }
 
-    @Override
-    public void initData() {
+        @Override
+        public void initData () {
 
-    }
-
-
-    @Override
-    public void showLoading() {
-
-    }
-
-    @Override
-    public void hideLoading() {
-
-    }
-
-    @Override
-    public void showMessage(@NonNull String message) {
-        checkNotNull(message);
-        UiUtils.SnackbarText(message);
-    }
-
-    @Override
-    public void launchActivity(@NonNull Intent intent) {
-        checkNotNull(intent);
-        UiUtils.startActivity(intent);
-    }
-
-    @Override
-    public void killMyself() {
-        finish();
-    }
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-        } else if (id == R.id.nav_gallery) {
-        } else if (id == R.id.nav_slideshow) {
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-
-        } else if (id == R.id.nav_send) {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
         }
-        return true;
-    }
 
-    @Override
-    public void onRefresh() {
+
+        @Override
+        public void showLoading () {
+
+        }
+
+        @Override
+        public void hideLoading () {
+
+        }
+
+        @Override
+        public void showMessage (@NonNull String message){
+            checkNotNull(message);
+            UiUtils.SnackbarText(message);
+        }
+
+        @Override
+        public void launchActivity (@NonNull Intent intent){
+            checkNotNull(intent);
+            UiUtils.startActivity(intent);
+        }
+
+        @Override
+        public void killMyself () {
+            finish();
+        }
+
+        @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem item){
+            switch (item.getItemId()) {
+                case R.id.navigation_mine:
+                    viewpages.setCurrentItem(0);
+                    break;
+                case R.id.navigation_jilv:
+                    viewpages.setCurrentItem(1);
+                    break;
+                case R.id.navigation_vedio:
+                    viewpages.setCurrentItem(2);
+                    break;
+                case R.id.navigation_image:
+                    viewpages.setCurrentItem(3);
+                    break;
+                case R.id.nav_item1:
+                    break;
+                case R.id.nav_item2:
+                    break;
+                case R.id.nav_item3:
+                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                    drawer.closeDrawer(GravityCompat.START);
+                    break;
+            }
+            return false;
+        }
+
+        @Override
+        public void onRefresh () {
+
+        }
 
     }
-}
